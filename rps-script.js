3dcda3c6079c9975371a1d990d45903548901715
nmaggiore <77024179+NickMagg@users.noreply.github.com>
2023-04-24 22:28:16 -0400

    Add compare functions

--- rps-script.js
+++ rps-script.js
@@ -1,13 +1,50 @@
-let computerSelection;
-let playerSelection
+const choices = ['Rock', 'Paper', 'Scissors'];
 
-const choices = ["Rock", "Paper", "Scissors"];
-
-function computerChoice() {
+function computerChoice(){
     return choices[Math.floor(Math.random()*choices.length)];
 }//* Determine computer selection
-computerSelection = computerChoice();
+
+const computerSelection = computerChoice();
+
 console.log(computerSelection);
 
-//* Prompt player for selection
-//* Compare selections and determine the winner
+let playerSelection = prompt("Rock, Paper or Scissors");//* Prompt player for selection
+
+let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+console.log(playerChoice);
+
+if (playerChoice == computerSelection){
+    alert("It's a tie!");
+}
+
+if (playerChoice === "Paper"){
+    if (computerSelection === "Scissors"){
+        alert("You LOST");
+    } else if (computerSelection === "Rock") {
+        alert("You WON!");
+    }
+}
+
+if (playerChoice === "Rock"){
+    if (computerSelection === "Paper"){
+        alert("You LOST");
+    } else if (computerSelection === "Scissors") {
+        alert("You WON!");
+    }
+}
+
+if (playerChoice === "Scissors"){
+    if (computerSelection === "Rock"){
+        alert("You LOST");
+    } else if (computerSelection === "Paper") {
+        alert("You WON!");
+    }
+}
+
+if (playerChoice != "Rock" &&
+    playerChoice != "Paper" && 
+    playerChoice != "Scissors"){
+    alert("That's not an option!");
+}
+    //* Compare selections and determine the winner
